85f845080e2b68bf6ae00b7bc818d1342644bf72
Giovanni Bortolussi <gbortolussi@degreed.com>
2023-04-14 12:39:55 +0200

    stream update

--- tap-getabstract/streams.py
+++ tap-getabstract/streams.py
@@ -3,9 +3,11 @@
 import logging
 from typing import cast, Dict, Optional, Any, Iterable
 from pathlib import Path
+from urllib.parse import parse_qsl
 from singer_sdk import typing as th
 from singer_sdk.streams import RESTStream
 from singer_sdk.authenticators import APIAuthenticatorBase, OAuthAuthenticator
+from singer_sdk.pagination import BaseHATEOASPaginator
 
 logging.basicConfig(level=logging.INFO)
 
@@ -13,6 +15,8 @@ SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 
 
 class GetabstractOAuthAuthenticator(OAuthAuthenticator):
+    """Authenticate with client credentials"""
+
     @property
     def oauth_request_body(self) -> dict:
         return {
@@ -22,19 +26,40 @@ class GetabstractOAuthAuthenticator(OAuthAuthenticator):
         }
 
 
+class MyHATEOASPaginator(BaseHATEOASPaginator):
+    """
+    Link 1: https://sdk.meltano.com/en/latest/classes/singer_sdk.pagination.BaseHATEOASPaginator.html
+    Link 2: https://sdk.meltano.com/en/latest/guides/pagination-classes.html#example-hateoas-pagination-a-k-a-next-links
+    """
+
+    def get_next_url(self, response):
+        data = response.json()
+        _links = data.get("_links")
+        if _links["next"]:
+            next = _links["next"]["href"]
+        else:
+            next = None
+        return next
+
+
 class TapGetabstractStream(RESTStream):
     """Getabstract stream class."""
 
     url_base = "https://www.getabstract.com/api"
 
+    def get_new_paginator(self):
+        return MyHATEOASPaginator()
+
     def get_url_params(
         self, context: Optional[dict], next_page_token: Optional[Any]
     ) -> Dict[str, Any]:
         """Return a dictionary of values to be used in URL parameterization."""
-        params = super().get_url_params(context, next_page_token)
-        params["psize"] = self.config["page_size"]
-        params["activeOnly"] = self.config["active_only"]
-        params["language"] = self.config["language"]
+        params = {}
+        params.update({"psize": self.config["page_size"]})
+        params.update({"activeOnly": self.config["active_only"]})
+        params.update({"language": self.config["language"]})
+        if next_page_token:
+            params.update(parse_qsl(next_page_token.query))
         return params
 
     @property
